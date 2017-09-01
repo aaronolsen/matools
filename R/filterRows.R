04f9c1db31eb90db61f24cb330b554d3446b7468
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-09-01 11:17:28 -0400

    Added output to save which rows were filtered out (for later replacement)

--- R/filterRows.R
+++ R/filterRows.R
@@ -2,15 +2,22 @@ filterRows <- function(rows, criteria){
 
 	for(criteria_name in names(criteria)){
 	
-		if(!criteria_name %in% names(rows)) stop("'", criteria_name, "' not found in rows names.")
+		if(!criteria_name %in% names(rows)) stop("'", criteria_name, "' not found in motion property names.")
 	
 		#
-		select_which <- which(rows[[criteria_name]] %in% criteria[[criteria_name]])
+		if(length(criteria[[criteria_name]]) > 1){
+			select_which <- which(rows[[criteria_name]] %in% criteria[[criteria_name]])
+		}else{
+			select_which <- which(grepl(criteria[[criteria_name]], rows[[criteria_name]]))
+		}
 		
 		#
-		initial_length <- length(criteria[[criteria_name]])
+		#initial_length <- length(criteria[[criteria_name]])
 		
 		for(rows_name in names(rows)){
+
+			# Internal field used with filterRows
+			if(rows_name %in% c('n.iter')) next
 			
 			rows_class <- class(rows[[rows_name]])
 
@@ -19,10 +26,9 @@ filterRows <- function(rows, criteria){
 			if(is.null(dim_rows)){
 				rows[[rows_name]] <- rows[[rows_name]][select_which]
 			}else{
-				
 				if(length(dim_rows) == 2){
-					if(dim_rows[1] == initial_length) rows[[rows_name]] <- rows[[rows_name]][select_which, ]
-					if(dim_rows[2] == initial_length) rows[[rows_name]] <- rows[[rows_name]][, select_which]
+					#if(dim_rows[1] == initial_length) rows[[rows_name]] <- rows[[rows_name]][select_which, ]
+					rows[[rows_name]] <- rows[[rows_name]][, select_which]
 				}else if(length(dim_rows) == 3){
 					rows[[rows_name]] <- rows[[rows_name]][, , select_which]
 				}else if(length(dim_rows) == 4){
@@ -32,7 +38,16 @@ filterRows <- function(rows, criteria){
 
 			class(rows[[rows_name]]) <- rows_class
 		}
-	}
 
+		# Save where rows were extracted in case they will be replaced
+		if(is.null(rows$replace.rows)){
+			rows$replace.rows <- select_which
+		}else{
+			rows$replace.rows <- rows$replace.rows[select_which]
+		}
+	}
+	
+	rows$n.iter <- length(rows$replace.rows)
+	
 	rows
 }
