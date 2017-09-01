9e590325f47e63fcac4cf36e9c5c800fa3861d2e
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-09-01 11:16:01 -0400

    Added animation control parameters

--- R/drawMotion.R
+++ R/drawMotion.R
@@ -1,30 +1,38 @@
-drawMotion <- function(file, pts, pts.path, pts.cex = 1, pts.col = 'black', path.col = 'black', 
-	path.lwd = 1, frame = TRUE, window.title = 'Draw Motion'){
+drawMotion <- function(file, pts, pts.path = NULL, pts.cex = 1, pts.col.stroke = 'black', 
+	pts.col.fill = 'black', path.col = 'black', path.lwd = 1, pts2 = NULL, pts2.cex = 1, 
+	pts2.col.stroke = 'red', pts2.col.fill = 'red', frame = TRUE, duration = 1, 
+	window.title = 'Draw Motion'){
 
-	svg.new(file, window.title=window.title)
-
-	# Get paths to connect
-	read_lines <- readLines(pts.path)
-	plist_names <- list()
-	for(line_num in 1:length(read_lines)) plist_names[[length(plist_names)+1]] <- strsplit(read_lines[line_num], ',[ ]?')[[1]]
+	svg.new(file, animate.duration=duration, window.title=window.title)
 
 	# Get xyz limits
 	ranges <- apply(pts, 2, 'range', na.rm=TRUE)
 
-	svg.points(pts, col.stroke=pts.col, col.fill=pts.col, cex=pts.cex)
+	# Draw first set of points
+	svg.points(pts, col.stroke=pts.col.stroke, col.fill=pts.col.fill, cex=pts.cex)
 
 	# Draw model paths
-	for(i in 1:length(plist_names)){
+	if(!is.null(pts.path)){
 
-		path_names <- plist_names[[i]][plist_names[[i]] %in% dimnames(pts)[[1]]]
-		if(length(path_names) > 0){
+		read_lines <- readLines(pts.path)
+		plist_names <- list()
+		for(line_num in 1:length(read_lines)) plist_names[[length(plist_names)+1]] <- strsplit(read_lines[line_num], ',[ ]?')[[1]]
+	
+		for(i in 1:length(plist_names)){
 
-			d <- c()
-			for(j in 1:length(path_names)) d <- c(d, which(path_names[j] == dimnames(pts)[[1]]))
+			path_names <- plist_names[[i]][plist_names[[i]] %in% dimnames(pts)[[1]]]
+			if(length(path_names) > 0){
+
+				d <- c()
+				for(j in 1:length(path_names)) d <- c(d, which(path_names[j] == dimnames(pts)[[1]]))
 			
-			svg.pathsC(path=d, col.stroke=path.col, lwd=path.lwd)
+				svg.pathsC(path=d, col.stroke=path.col, lwd=path.lwd)
+			}
 		}
 	}
+
+	# Draw second set of points
+	svg.points(pts2, col.stroke=pts2.col.stroke, col.fill=pts2.col.fill, cex=pts2.cex)
 
 	# Draw frame
 	if(frame) svg.frame(ranges, z.index=-1)
