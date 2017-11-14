38d1e29d57486a4d249a8d9861d3358e74b5c4bf
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-11-13 19:38:03 -0500

    Added parameter to change which array dimension ends up as the matrix rows

--- R/arr2mat.R
+++ R/arr2mat.R
@@ -1,4 +1,4 @@
-arr2mat <- function(arr, xyz=c('_X', '_Y', '_Z')){
+arr2mat <- function(arr, xyz=c('_X', '_Y', '_Z'), dim.to.row=3){
 
 	# Get dimensions
 	dim_arr <- dim(arr)
@@ -6,18 +6,34 @@ arr2mat <- function(arr, xyz=c('_X', '_Y', '_Z')){
 	# Dimnames
 	dimnames_arr <- dimnames(arr)
 
-	# Repeate landmark names
-	lm_repeat <- c(t(matrix(dimnames_arr[[1]], nrow=length(dimnames_arr[[1]]), ncol=3)))
-
 	#print(paste0(lm_repeat, '.', xyz))
 	#print(list(dimnames_arr[[3]], paste0(dimnames_arr[[1]], '.', letters[24:26])))
 	
 	# Convert to matrix
-	mat <- matrix(NA, nrow=dim_arr[3], ncol=dim_arr[1]*dim_arr[2], 
-		dimnames=list(NULL, paste0(lm_repeat, xyz)))
+	if(dim.to.row == 3){
+
+		# Repeate first dimension names
+		lm_repeat <- c(t(matrix(dimnames_arr[[1]], nrow=length(dimnames_arr[[1]]), ncol=3)))
+
+		# Create matrix
+		mat <- matrix(NA, nrow=dim_arr[3], ncol=dim_arr[1]*dim_arr[2], 
+			dimnames=list(NULL, paste0(lm_repeat, xyz)))
 	
-	# Fill matrix
-	for(i in 1:nrow(mat)) mat[i, ] <- t(arr[, , i])
+		# Fill matrix
+		for(i in 1:nrow(mat)) mat[i, ] <- t(arr[, , i])
+
+	}else if(dim.to.row == 1){
+
+		# Repeate third dimension names
+		lm_repeat <- c(t(matrix(dimnames_arr[[3]], nrow=length(dimnames_arr[[3]]), ncol=3)))
+
+		# Create matrix
+		mat <- matrix(NA, nrow=dim_arr[1], ncol=dim_arr[2]*dim_arr[3], 
+			dimnames=list(dimnames_arr[[1]], paste0(lm_repeat, xyz)))
+	
+		# Fill matrix
+		for(i in 1:nrow(mat)) mat[i, ] <- arr[i, , ]
+	}
 
 	mat
 }
