eb6595f3d85fdb79ed8f2e5ec755e2c895828bcd
Aaron Olsen <aaronolsen@users.noreply.github.com>
2017-11-14 21:19:21 -0500

    Added writing via data.frame so that character data have quotes and numerical and logical data do not

--- R/writeMotion.R
+++ R/writeMotion.R
@@ -3,7 +3,7 @@ writeMotion <- function(x, file){
 	# Create write mat
 	write_mat <- NULL
 
-	if(is.null(names(x))){
+	if(is.matrix(x)){
 
 		write_mat <- x
 	}else{
@@ -12,31 +12,49 @@ writeMotion <- function(x, file){
 	
 			# Internal field used with filterRows
 			if(xn %in% c('replace.rows', 'n.iter')) next
-	
+
 			if('tmat' %in% class(x[[xn]]) || xn == 'tmat'){
 			
 				# Convert array of transformation matrices to matrix
-				tmat <- tmarr2mat(x[[xn]])
+				tmat <- as.data.frame(tmarr2mat(x[[xn]]))
 			
 				# Add columns
 				write_mat <- cbind(write_mat, tmat)
 
+				# Add columns
+				if(is.null(write_mat)){
+					write_mat <- tmat
+				}else{
+					write_mat <- cbind(write_mat, tmat)
+				}
+
 			}else if('xyz' %in% class(x[[xn]]) || xn == 'xyz'){
 
 				# Convert array of points to matrix
-				xyz <- arr2mat(x[[xn]])
-			
+				xyz <- as.data.frame(arr2mat(x[[xn]]))
+
 				# Add columns
-				write_mat <- cbind(write_mat, xyz)
+				if(is.null(write_mat)){
+					write_mat <- xyz
+				}else{
+					write_mat <- cbind(write_mat, xyz)
+				}
 
 			}else{
+			
+				# If is list, unlist
+				if(is.list(x[[xn]])) x[[xn]] <- unlist(x[[xn]])
 
 				# Add columns
-				write_mat <- cbind(write_mat, matrix(x[[xn]], ncol=1, dimnames=list(NULL, xn)))
+				if(is.null(write_mat)){
+					write_mat <- as.data.frame(matrix(x[[xn]], ncol=1, dimnames=list(NULL, xn)))
+				}else{
+					write_mat <- cbind(write_mat, as.data.frame(matrix(x[[xn]], ncol=1, dimnames=list(NULL, xn))))
+				}
 			}
 		}
 	}
-	
+
 	if(grepl('[.]csv$', file)) write.csv(x=write_mat, file=file, row.names=FALSE)
 	
 	NULL
