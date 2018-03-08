567f145667854eb80e4343833108984bd16a76ae
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-03-07 22:14:11 -0500

    Fixed bug in conversion to array

--- R/mat2arr.R
+++ R/mat2arr.R
@@ -1,14 +1,29 @@
 mat2arr <- function(mat, pattern = '[.|_](x|y|z)$', ignore.case = TRUE){
 
-	# Create array
-	arr <- array(NA, dim=c(ncol(mat)/3,3,nrow(mat)), 
-		dimnames=list(unique(gsub(pattern, '', colnames(mat), ignore.case=ignore.case)), NULL, rownames(mat)))
+	if(pattern == '[.|_](x|y)$'){
 
-	# Question marks at end of marker names become '.'
-	dimnames(arr)[[1]] <- gsub('[.]$', '', dimnames(arr)[[1]])
+		# Create array
+		arr <- array(NA, dim=c(ncol(mat)/2,2,nrow(mat)), 
+			dimnames=list(unique(gsub(pattern, '', colnames(mat), ignore.case=ignore.case)), NULL, rownames(mat)))
 
-	# Fill array
-	for(i in 1:dim(arr)[3]) arr[, , i] <- matrix(mat[i, ], nrow=ncol(mat)/3, ncol=3, byrow=TRUE)
+		# Question marks at end of marker names become '.'
+		dimnames(arr)[[1]] <- gsub('[.]$', '', dimnames(arr)[[1]])
+
+		# Fill array
+		for(i in 1:dim(arr)[3]) arr[, , i] <- matrix(mat[i, ], nrow=ncol(mat)/2, ncol=2, byrow=TRUE)
+
+	}else{
+
+		# Create array
+		arr <- array(NA, dim=c(ncol(mat)/3,3,nrow(mat)), 
+			dimnames=list(unique(gsub(pattern, '', colnames(mat), ignore.case=ignore.case)), NULL, rownames(mat)))
+
+		# Question marks at end of marker names become '.'
+		dimnames(arr)[[1]] <- gsub('[.]$', '', dimnames(arr)[[1]])
+
+		# Fill array
+		for(i in 1:dim(arr)[3]) arr[, , i] <- matrix(mat[i, ], nrow=ncol(mat)/3, ncol=3, byrow=TRUE)
+	}
 	
 	arr
 }
