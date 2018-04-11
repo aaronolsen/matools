f275cb5dc5d54be00f41c51c4a93e4623d974e37
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-04-10 23:27:21 -0400

    Added check for 2 vs 3 dimensions

--- R/arr2mat.R
+++ R/arr2mat.R
@@ -13,11 +13,11 @@ arr2mat <- function(arr, xyz=c('_X', '_Y', '_Z'), dim.to.row=3){
 	if(dim.to.row == 3){
 
 		# Repeate first dimension names
-		lm_repeat <- c(t(matrix(dimnames_arr[[1]], nrow=length(dimnames_arr[[1]]), ncol=3)))
+		lm_repeat <- c(t(matrix(dimnames_arr[[1]], nrow=length(dimnames_arr[[1]]), ncol=dim(arr)[2])))
 
 		# Create matrix
 		mat <- matrix(NA, nrow=dim_arr[3], ncol=dim_arr[1]*dim_arr[2], 
-			dimnames=list(NULL, paste0(lm_repeat, xyz)))
+			dimnames=list(dimnames_arr[[3]], paste0(lm_repeat, xyz[1:dim(arr)[2]])))
 	
 		# Fill matrix
 		for(i in 1:nrow(mat)) mat[i, ] <- t(arr[, , i])
@@ -25,11 +25,11 @@ arr2mat <- function(arr, xyz=c('_X', '_Y', '_Z'), dim.to.row=3){
 	}else if(dim.to.row == 1){
 
 		# Repeate third dimension names
-		lm_repeat <- c(t(matrix(dimnames_arr[[3]], nrow=length(dimnames_arr[[3]]), ncol=3)))
+		lm_repeat <- c(t(matrix(dimnames_arr[[3]], nrow=length(dimnames_arr[[3]]), ncol=dim(arr)[2])))
 
 		# Create matrix
 		mat <- matrix(NA, nrow=dim_arr[1], ncol=dim_arr[2]*dim_arr[3], 
-			dimnames=list(dimnames_arr[[1]], paste0(lm_repeat, xyz)))
+			dimnames=list(dimnames_arr[[1]], paste0(lm_repeat, xyz[1:dim(arr)[2]])))
 	
 		# Fill matrix
 		for(i in 1:nrow(mat)) mat[i, ] <- arr[i, , ]
