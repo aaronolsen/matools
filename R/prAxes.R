e58dcb084c16dfc6d8f114d30332334c07d7f24a
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-04-20 23:40:46 -0400

    Added function to find principal axes of a set of points

---
+++ R/prAxes.R
@@ -0,0 +1,21 @@
+prAxes <- function(pts){
+
+	# PC analysis
+	pca <- prcomp(pts)
+	
+	# Find centroid
+	meanX <- apply(pts, 2, mean) 
+
+	# Get points at ends of each axis
+	axes <- list()
+	ends <- matrix(NA, nrow=2*ncol(pca$x), ncol=length(meanX))
+
+	j <- 1
+	for(i in 1:ncol(pca$x)){
+		axes[[i]] <- rbind(meanX + min(pca$x[, i])*pca$rotation[, i], meanX + max(pca$x[, i])*pca$rotation[, i])
+		ends[j:(j+1), ] <- axes[[i]]
+		j <- j + 2
+	}
+
+	list('axes'=axes, 'ends'=ends)
+}
