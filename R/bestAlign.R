6b9c957dbc4e41a16b8600e6fdda197aba10e55e
Aaron Olsen <aaronolsen@users.noreply.github.com>
2018-05-14 13:44:03 -0400

    Added option for 2-point alignment

--- R/bestAlign.R
+++ R/bestAlign.R
@@ -84,59 +84,84 @@ bestAlign <- function(m1, m2, m3 = NULL, sign = NULL){
 	m1oc <- scale(m1o, center=TRUE, scale=FALSE)
 	m2oc <- scale(m2o, center=TRUE, scale=FALSE)
 
-	# Treat as two vectors
+	# Find best alignment with just two points
 	if(nrow(m1oc) == 2){
-	}
-
-	# FIND ROTATION MATRIX TO APPLY TO M2 THAT MINIMIZES DISTANCE BETWEEN M1 AND M2
-	SVD <- svd(t(na.omit(m1oc)) %*% na.omit(m2oc))
-
-	# CORRECTION TO ENSURE A RIGHT-HANDED COORDINATE SYSTEM
-	S <- diag(3)
-	S[3,3] <- sign(det(SVD$v %*% t(SVD$u)))
-
-	# GET ROTATION MATRIX
-	# MIGHT CHANGE POINTS RELATIVE TO ONE ANOTHER (VERY SLIGHTLY)
-	# I THINK IT ONLY HAPPENS WHEN ONE DIMENSION OF M1 IS ALL ZEROS
-	# CAUSES PROBLEM IN DETERMINING FIT PERHAPS
-	RM <- SVD$v %*% S %*% t(SVD$u)
-
-	# CREATE ROTATION TRANSFORMATION MATRIX
-	tmat2 <- diag(4);tmat2[1:3, 1:3] <- t(RM)
-
-	# TEST ALIGNMENT
-	#t2 <- m2c %*% RM
-	#print(m1oc[!is.na(m1oc[, 1]), ])
-	#print(t2[!is.na(t2[, 1]), ])
-
-	# ROTATE ALL CENTER LANDMARKS IN M2
-	m2r <- mtransform(m2c, tmat2)
-	if(!is.null(m3)) m3r <- mtransform(m3c, tmat2)
-
-	# TEST WHETHER CHIRALITY OF POINT SET HAS FLIPPED
-	if(nrow(m1o) == 3 && sum(!is.na(m2[,1])) > 3){
 		
-		# IF THE ALIGNMENT FLIPPED THE SET TO ITS MIRROR IMAGE, THE CROSS PRODUCT OF THE 
-		#	FIRST THREE POINTS WILL MAINTAIN THE SAME ORIENTATION. BUT ANY OTHER POINTS WILL
-		#	BE FLIPPED RELATIVE TO THIS VECTOR. SO IF THE DISTANCE BETWEEN THE CROSS PRODUCT
-		#	AND THESE POINTS CHANGES, IT INDICATES THE CHIRALITY HAS BEEN FLIPPED
+		# Check if points are identical
+		tmat2 <- diag(4)
+		if(sum(colMeans(abs(m2oc - m1oc))) > 1e-10){
 
-		# FIND NORMAL VECTORS FOR PRE AND POST ROTATED SETS
-		m2c_cprod <- uvector_ma(cprod_ma(m2c[2, ]-m2c[1, ], m2c[3, ]-m2c[1, ]))
-		m2r_cprod <- uvector_ma(cprod_ma(m2r[2, ]-m2r[1, ], m2r[3, ]-m2r[1, ]))
-		
-		# FIND DISTANCE FROM CPROD VECTOR TO OTHER POINTS
-		dpp <- dppt(m2c_cprod, m2c[4:min(7,nrow(m2c)), ])
-		dpp_r <- dppt(m2r_cprod, m2r[4:min(7,nrow(m2r)), ])
-		
-		# CHIRALITY HAS FLIPPED, FLIP 3RD COLUMN OF SVD$v AND RE-TRANSFORM
-		if(sum(round(abs(dpp - dpp_r), 7)) > 0.001){
-			SVD$v[, 3] <- -SVD$v[, 3]
-			RM <- SVD$v %*% S %*% t(SVD$u)
+			m_axis <- cprod(m1oc[2,]-m1oc[1,], m2oc[2,]-m2oc[1,])
+
+			# Find angle between points
+			m_avec <- avec(m2oc[2,], m1oc[2,], axis=m_axis, about.axis=TRUE)
+
+			# Find rotation
+			RM <- tMatrixEP(v=m_axis, -m_avec)
+			
+			#rotated <- rotateBody(m2oc, m_axis, -m_avec)
+			#cat('-----------\n')
+			#print(m1oc)
+			#print(rotated)
+
+			# Set rotation matrix			
 			tmat2[1:3, 1:3] <- t(RM)
-			m2r <- mtransform(m2c, tmat2)
-			if(!is.null(m3)) m3r <- mtransform(m3c, tmat2)
-		}else{
+		}
+
+		m2r <- mtransform(m2c, tmat2)
+
+	}else{
+
+		# FIND ROTATION MATRIX TO APPLY TO M2 THAT MINIMIZES DISTANCE BETWEEN M1 AND M2
+		SVD <- svd(t(na.omit(m1oc)) %*% na.omit(m2oc))
+
+		# CORRECTION TO ENSURE A RIGHT-HANDED COORDINATE SYSTEM
+		S <- diag(3)
+		S[3,3] <- sign(det(SVD$v %*% t(SVD$u)))
+
+		# GET ROTATION MATRIX
+		# MIGHT CHANGE POINTS RELATIVE TO ONE ANOTHER (VERY SLIGHTLY)
+		# I THINK IT ONLY HAPPENS WHEN ONE DIMENSION OF M1 IS ALL ZEROS
+		# CAUSES PROBLEM IN DETERMINING FIT PERHAPS
+		RM <- SVD$v %*% S %*% t(SVD$u)
+
+		# CREATE ROTATION TRANSFORMATION MATRIX
+		tmat2 <- diag(4);tmat2[1:3, 1:3] <- t(RM)
+
+		# TEST ALIGNMENT
+		#t2 <- m2c %*% RM
+		#print(m1oc[!is.na(m1oc[, 1]), ])
+		#print(t2[!is.na(t2[, 1]), ])
+
+		# ROTATE ALL CENTER LANDMARKS IN M2
+		m2r <- mtransform(m2c, tmat2)
+		if(!is.null(m3)) m3r <- mtransform(m3c, tmat2)
+
+		# TEST WHETHER CHIRALITY OF POINT SET HAS FLIPPED
+		if(nrow(m1o) == 3 && sum(!is.na(m2[,1])) > 3){
+		
+			# IF THE ALIGNMENT FLIPPED THE SET TO ITS MIRROR IMAGE, THE CROSS PRODUCT OF THE 
+			#	FIRST THREE POINTS WILL MAINTAIN THE SAME ORIENTATION. BUT ANY OTHER POINTS WILL
+			#	BE FLIPPED RELATIVE TO THIS VECTOR. SO IF THE DISTANCE BETWEEN THE CROSS PRODUCT
+			#	AND THESE POINTS CHANGES, IT INDICATES THE CHIRALITY HAS BEEN FLIPPED
+
+			# FIND NORMAL VECTORS FOR PRE AND POST ROTATED SETS
+			m2c_cprod <- uvector_ma(cprod_ma(m2c[2, ]-m2c[1, ], m2c[3, ]-m2c[1, ]))
+			m2r_cprod <- uvector_ma(cprod_ma(m2r[2, ]-m2r[1, ], m2r[3, ]-m2r[1, ]))
+		
+			# FIND DISTANCE FROM CPROD VECTOR TO OTHER POINTS
+			dpp <- dppt(m2c_cprod, m2c[4:min(7,nrow(m2c)), ])
+			dpp_r <- dppt(m2r_cprod, m2r[4:min(7,nrow(m2r)), ])
+		
+			# CHIRALITY HAS FLIPPED, FLIP 3RD COLUMN OF SVD$v AND RE-TRANSFORM
+			if(sum(round(abs(dpp - dpp_r), 7)) > 0.001){
+				SVD$v[, 3] <- -SVD$v[, 3]
+				RM <- SVD$v %*% S %*% t(SVD$u)
+				tmat2[1:3, 1:3] <- t(RM)
+				m2r <- mtransform(m2c, tmat2)
+				if(!is.null(m3)) m3r <- mtransform(m3c, tmat2)
+			}else{
+			}
 		}
 	}
 
@@ -158,6 +183,8 @@ bestAlign <- function(m1, m2, m3 = NULL, sign = NULL){
 
 	# GET FINAL TRANSFORMATION MATRIX
 	tmat <- tmat3 %*% tmat2 %*% tmat1
+
+	#errors <- m1o - mtransform(m2o, tmat)
 	
 	list(
 		mat=m2r,
