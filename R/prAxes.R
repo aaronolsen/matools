e69f017d3d8ce46e8d573336484081b571234b36
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-04-13 15:31:58 -0400

    Added vectors to output

--- R/prAxes.R
+++ R/prAxes.R
@@ -9,13 +9,15 @@ prAxes <- function(pts){
 	# Get points at ends of each axis
 	axes <- list()
 	ends <- matrix(NA, nrow=2*ncol(pca$x), ncol=length(meanX))
+	vectors <- matrix(NA, nrow=ncol(pca$x), ncol=length(meanX))
 
 	j <- 1
 	for(i in 1:ncol(pca$x)){
 		axes[[i]] <- rbind(meanX + min(pca$x[, i])*pca$rotation[, i], meanX + max(pca$x[, i])*pca$rotation[, i])
 		ends[j:(j+1), ] <- axes[[i]]
+		vectors[i,] <- uvector_ma(axes[[i]][2,]-axes[[i]][1,])
 		j <- j + 2
 	}
-
-	list('axes'=axes, 'ends'=ends)
+	
+	list('axes'=axes, 'ends'=ends, 'vectors'=vectors)
 }
