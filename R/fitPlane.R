b74ad01a655442a9d73335729016b20ffb7dd7d0
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-05-17 11:57:48 -0400

    Created fitPlane

---
+++ R/fitPlane.R
@@ -0,0 +1,45 @@
+fitPlane <- function(mat){
+	
+	# Center by centroid
+	centroid.align <- mat - matrix(colMeans(mat), nrow(mat), ncol(mat), byrow=TRUE)
+
+	# Check for differences in point position
+	if(mean(apply(centroid.align, 2, 'sd')) < 1e-10) return(NULL)
+
+	# SVD of centroid-aligned points
+	meanX <- apply(mat, 2, mean) 
+	pca <- prcomp(mat)
+
+	v <- matrix(NA, nrow=3, ncol=3)
+	endpts <- array(NA, dim=c(2,3,3))
+	side_len <- rep(NA, 3)
+
+	for(i in 1:3){
+
+		t <- range(pca$x[, i])
+
+		# Get end points along axis
+		endpts[1,,i] <- meanX + t[1]*pca$rotation[, i]
+		endpts[2,,i] <- meanX + t[2]*pca$rotation[, i]
+
+		# Get vector corresponding to axis
+		v[i, ] <- uvector(endpts[2,,i]-endpts[1,,i])
+
+		# Get half of length
+		side_len[i] <- distPointToPoint(endpts[1,,i], endpts[2,,i])
+	}
+
+	half_side_len <- side_len / 2
+	corners_center <- colMeans(rbind(endpts[,,1], endpts[,,2], endpts[,,3]))
+	corners <- matrix(NA, 4, 3, byrow=TRUE)
+	corners[1,] <- corners_center + half_side_len[1]*v[1,] + half_side_len[2]*v[2,]
+	corners[2,] <- corners_center - half_side_len[1]*v[1,] + half_side_len[2]*v[2,]
+	corners[3,] <- corners_center - half_side_len[1]*v[1,] - half_side_len[2]*v[2,]
+	corners[4,] <- corners_center + half_side_len[1]*v[1,] - half_side_len[2]*v[2,]
+
+	shape_obj <- list('N'=v[3, ], 'Q'=corners_center, 'center'=corners_center, 
+		'corners'=corners, 'vectors'=v, 'len'=side_len[1:2], 'halflen'=side_len[1:2]/2)
+	class(shape_obj) <- 'plane'
+
+	shape_obj
+}
