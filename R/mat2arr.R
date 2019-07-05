b52272b42fa51e0ce5e3781e0b74f9d67cf5e934
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-07-05 11:54:38 -0400

    Added check for duplicate names

--- R/mat2arr.R
+++ R/mat2arr.R
@@ -14,9 +14,21 @@ mat2arr <- function(mat, pattern = '[.|_](x|y|z)$', ignore.case = TRUE){
 
 	}else{
 
+		# Check for duplicate names
+		gsub_names <- gsub(pattern, '', colnames(mat), ignore.case=ignore.case)
+		gsub_names_unique <- unique(gsub_names)
+		
+		if((length(gsub_names) / 3) > length(gsub_names_unique)){
+			duplicates <- c()
+			for(i in 1:length(gsub_names_unique)){
+				if(sum(gsub_names_unique[i] == gsub_names) > 3) duplicates <- c(duplicates, gsub_names_unique[i])
+			}
+			stop('"mat" input has duplicate column names ("', paste0(duplicates, collapse=', '), '").')
+		}
+
 		# Create array
 		arr <- array(NA, dim=c(ncol(mat)/3,3,nrow(mat)), 
-			dimnames=list(unique(gsub(pattern, '', colnames(mat), ignore.case=ignore.case)), NULL, rownames(mat)))
+			dimnames=list(gsub_names_unique, NULL, rownames(mat)))
 
 		# Question marks at end of marker names become '.'
 		dimnames(arr)[[1]] <- gsub('[.]$', '', dimnames(arr)[[1]])
