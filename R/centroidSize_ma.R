b3b4f030c990e9d3b571100abcddc819914e578b
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-11-07 17:21:21 -0500

    Create centroidSize_ma.R

---
+++ R/centroidSize_ma.R
@@ -0,0 +1,29 @@
+centroidSize_ma <- function(m, na.rm = TRUE, transpose = FALSE){
+
+	if(is.array(m) && length(dim(m)) > 2){
+		r <- rep(NA, dim(m)[3])
+		for(i in 1:dim(m)[3]){
+
+			n <- na.omit(m[,, i])
+		
+			# CENTER
+			n <- n - matrix(colMeans(n, na.rm=na.rm), nrow=nrow(n), ncol=ncol(n), byrow=TRUE)
+		
+			# FIND CENTROID SIZE
+			r[i] <- sqrt(sum(n^2))
+		}
+		return(r)
+	}else{
+		
+		if(transpose) m <- t(m)
+	
+		m <- na.omit(m)
+	
+		# CENTER
+		m <- m - matrix(colMeans(m, na.rm=na.rm), nrow=nrow(m), ncol=ncol(m), byrow=TRUE)
+	
+		# FIND CENTROID SIZE
+		r <- sqrt(sum(m^2))
+	}
+	r
+}
