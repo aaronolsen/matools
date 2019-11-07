290619e7fe8db9a352d48772fcbca6896dbad591
Aaron Olsen <aaronolsen@users.noreply.github.com>
2019-11-07 17:21:38 -0500

    Updated to work with new cpp functions

--- R/mtransform.R
+++ R/mtransform.R
@@ -1,15 +1,15 @@
 mtransform <- function(mat, tmat){
 	
-	if(!is.matrix(mat)){
-		mat <- matrix(mat, nrow=1, ncol=length(mat))
-		dimnames_mat <- NULL
-	}else{
-		dimnames_mat <- dimnames(mat)
-	}
+	if(!is.matrix(mat)) mat <- matrix(mat, nrow=1, ncol=length(mat))
 
-	pmat <- matrix(1, nrow(mat), ncol(mat)+1)
-	pmat[, 1:3] <- mat
-	mat <- t(tmat %*% t(pmat))[, 1:3]
-	dimnames(mat) <- dimnames_mat
-	mat
+	#pmat <- matrix(1, nrow(mat), ncol(mat)+1)
+	#pmat[, 1:3] <- mat
+	#mat <- t(tmat %*% t(pmat))[, 1:3]
+
+	# Transform using C++ function
+	return_mat <- transform_pts(mat, tmat)
+
+	dimnames(return_mat) <- dimnames(mat)
+	
+	return_mat
 }
